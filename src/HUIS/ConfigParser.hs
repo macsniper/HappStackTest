60fb04fab20e341ae6d18ac7a2f050de721934b9
Clemens Mayer <macsniper@gmail.com>
2010-04-14 00:31:52 +0200

    re-cabalized and removed old config parser. TODO: use new config parser

--- src/HUIS/ConfigParser.hs
+++ src/HUIS/ConfigParser.hs
@@ -1,63 +1,54 @@
 module HUIS.ConfigParser where
- import Char
- import Control.Monad
- import qualified Data.Map as Map
- import Text.ParserCombinators.Parsec
- import Data.Either
- import Data.Maybe
+import Char
+import Control.Monad
+import qualified Data.Map as Map
+import Text.ParserCombinators.Parsec
+import Data.Either
+import Data.Maybe
 
- type Config = Map.Map String String
+type Config = Map.Map String String
 
 
- ident :: Parser String
- ident = do c <- letter <|> char '_'
-            cs <- many (letter <|> digit <|> char '_')
-            return (c:cs)
-       <?> "identifier"
- 
- 
- comment :: Parser ()
- comment = do char '#'
-              skipMany (noneOf "\r\n")
-              <?> "comment"
- 
- eol :: Parser ()
- eol = do oneOf "\n\r"
-          return ()
-     <?> "end of line"
-     
-     
-     
- item :: Parser (String, String)
- item = do key <- ident
-           skipMany space
-           char '='
-           skipMany space
-           value <- manyTill anyChar (try eol <|> try comment <|> eof)
-           return (key, rstrip value)
-     where rstrip = reverse . dropWhile isSpace . reverse  --remove leading whitespace...
-     
-     
- line :: Parser (Maybe (String, String))
- line = do skipMany space
-           try (comment >> return Nothing) <|> (item >>= return . Just)
-           
-           
- file :: Parser [(String, String)]
- file = do lines <- many line
-           return (catMaybes lines)
-           
-           
- readConfig :: SourceName -> IO (Either ParseError Config)
- readConfig name =
-     parseFromFile file name >>=
-     return . fmap (foldr (uncurry Map.insert) Map.empty . reverse)
-           
+ident :: Parser String
+ident = do c <- letter <|> char '_'
+           cs <- many (letter <|> digit <|> char '_')
+           return (c:cs)
+      <?> "identifier"
 
-        
-     
-     
-     
-     
-     
- 
+
+comment :: Parser ()
+comment = do char '#'
+             skipMany (noneOf "\r\n")
+             <?> "comment"
+
+eol :: Parser ()
+eol = do oneOf "\n\r"
+         return ()
+    <?> "end of line"
+
+
+
+item :: Parser (String, String)
+item = do key <- ident
+          skipMany space
+          char '='
+          skipMany space
+          value <- manyTill anyChar (try eol <|> try comment <|> eof)
+          return (key, rstrip value)
+    where rstrip = reverse . dropWhile isSpace . reverse  --remove leading whitespace...
+
+
+line :: Parser (Maybe (String, String))
+line = do skipMany space
+          try (comment >> return Nothing) <|> (item >>= return . Just)
+
+
+file :: Parser [(String, String)]
+file = do lines <- many line
+          return (catMaybes lines)
+           
+           
+readConfig :: SourceName -> IO (Either ParseError Config)
+readConfig name =
+    parseFromFile file name >>=
+    return . fmap (foldr (uncurry Map.insert) Map.empty . reverse)
--- src/HUIS/Server.hs
+++ src/HUIS/Server.hs
@@ -14,45 +14,4 @@ import Data.Map
 
 main = do
   conf <- readConfig "huis.conf"
-  port <- parseConfigFile "port"
-  
-  -- todo: put conf into single Map
-  simpleHTTP (Conf (read port::Int) Nothing) (runDispatcher)
-
-parseConfigFile:: String-> IO String
-parseConfigFile name = do
-  -- IO stuff :(
-  val <- readfile emptyCP "huis.conf"
-  let confparser = forceEither val
-      initialvalue = show ((forceEither $ get confparser "DEFAULT" name):: String)
-      -- do some regex-replacement-stuff (strip "")
-      actualvalue = subRegex (mkRegex "\"") initialvalue ""
-  return (actualvalue)
-
--- outdated code
-{-
-testapp:: ServerPart Response
-testapp = msum 
-  [ methodOnly GET  >> nullDir >> handleRequest "your Message!"
-  , methodOnly POST >> withData (showRequest) 
-  , methodOnly GET  >> path (handleRequest)]
-
-handleRequest:: String-> ServerPart Response
-handleRequest test =
-  ok $ toResponse $ showTestContent test
-
-showTestContent:: String-> Html
-showTestContent t = gui "/" << 
-  [label << t
-  , br
-  , textfield "epicfail" ! [size "50", value $ t]
-  , br
-  , submit "update" "gogogo"]
-  
-showRequest:: String-> ServerPart Response
-showRequest test =
-  ok $ toResponse $ body << 
-    [ stringToHtml test
-    , br
-    , anchor ! [href "/" ] << "back"]
--}
+  simpleHTTP (Conf 8080 Nothing) (runDispatcher)
