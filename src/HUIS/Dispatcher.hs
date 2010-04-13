c5f350db3618e0460d0f3e80798bdf037333a9ae
Clemens Mayer <macsniper@gmail.com>
2010-04-13 23:31:03 +0200

    added _simple_ authentification (http basic auth with hardcoded password)

--- src/HUIS/Dispatcher.hs
+++ src/HUIS/Dispatcher.hs
@@ -2,13 +2,13 @@ module HUIS.Dispatcher where
 
 import Happstack.Server
 import Control.Monad (msum)
-
+import qualified Data.Map
 import HUIS.StaticResponses
 
 runDispatcher:: ServerPart Response
 runDispatcher = msum 
-  -- GET '/' => show start page
-  [ methodOnly GET  >> nullDir >> showStartPage
+  -- GET '/' => show start page | simple authentification
+  [ basicAuth "HUIS" (Data.Map.fromList [("admin", "password")]) (methodOnly GET  >> nullDir >> showStartPage)
   -- GET '/ressources/img/*' => serve image-file
   , methodOnly GET >> dir "ressources" ( dir "img" ( path (showFile "img")))
   -- GET '/ressources/css/*' => serve css-file
