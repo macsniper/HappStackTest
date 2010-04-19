e17a05818126c99146592c50d1874101a4ee9a07
Clemens Mayer <macsniper@gmail.com>
2010-04-19 23:01:08 +0200

    refactoring, removed qualified import.

--- src/HUIS/Server.hs
+++ src/HUIS/Server.hs
@@ -21,20 +21,20 @@ main = do
 -- initialises the gitit-wiki and returns the configuration-file
 getWikiConfiguration::String-> IO Network.Gitit.Config
 getWikiConfiguration resdir = do
-  conf <- Network.Gitit.getDefaultConfig
+  conf <- getDefaultConfig
   tpldir' <- getDataDir
-  let tpldir = (tpldir' ++ resdir)
+  let tpldir = tpldir' ++ resdir
       conf' = conf{ --authHandler     = myAuthHandler
                   --, withUser        = myWithUser,
-                    repositoryPath  = (tpldir ++ "/wiki/repo")
-                  , repositoryType  = Network.Gitit.Git
-                  , defaultPageType = Network.Gitit.Markdown
-                  , templatesDir    = (tpldir ++ "/wiki/tpl") --does not work yet :(
-                  , cacheDir        = (tpldir ++ "/wiki/cache")
-                  , staticDir       = (tpldir ++ "/wiki/static")}
-  Network.Gitit.createStaticIfMissing conf'
-  Network.Gitit.createRepoIfMissing conf'
-  Network.Gitit.createTemplateIfMissing conf'
-  Network.Gitit.initializeGititState conf'
+                    repositoryPath  = tpldir ++ "/wiki/repo"
+                  , repositoryType  = Git
+                  , defaultPageType = Markdown
+                  , templatesDir    = tpldir ++ "/wiki/tpl" 
+                  , cacheDir        = tpldir ++ "/wiki/cache"
+                  , staticDir       = tpldir ++ "/wiki/static"}
+  createStaticIfMissing conf'
+  createRepoIfMissing conf'
+  createTemplateIfMissing conf'
+  initializeGititState conf'
   return conf'
  
