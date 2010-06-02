73c96dfd07f7ed540870b8ef5a77a5a67dd84bab
Clemens Mayer <macsniper@gmail.com>
2010-06-02 17:39:47 +0200

    added database connectivity.

---
+++ src/HUIS/Database.hs
@@ -0,0 +1,17 @@
+module HUIS.Database (connectDatabase, selectQuery) where
+
+
+import Database.HSQL
+import Database.HSQL.ODBC
+import Database.HSQL.Types
+import HUIS.ConfigParser(Config)
+import Data.Map
+
+
+connectDatabase:: Config -> IO Connection
+connectDatabase config = connect (config ! "dbsource") (config ! "user") (config ! "dbpass")
+
+selectQuery:: String -> Connection -> IO Statement
+selectQuery querystring conn = do
+  rows <- query conn querystring
+  return rows
--- src/HUIS/Server.hs
+++ src/HUIS/Server.hs
@@ -3,27 +3,35 @@ module HUIS.Server where
 
 import HUIS.Dispatcher
 import HUIS.ConfigParser
+import HUIS.Database
 import Happstack.Server
 import Text.Regex
 import Data.Map
 import Data.Maybe
 import Network.Gitit
+import System.Posix.User (setUserID, UserEntry(..), getUserEntryForName)
+import Control.Concurrent
+import System.Posix.Process (createSession)
 import Paths_HUIS
 
 main = do
   cfile <- getDataFileName "huis.conf"
   cdir <- getDataDir
   conf <- readConfig cfile
-  putStrLn $ "Starting up HUIS on 0.0.0.0:" ++ (conf ! "port") ++ "..."
   let rdir = conf ! "ressourcedir"
       wikidir = conf ! "wikidir"
       conf' = update (\x-> Just (cdir ++ (tail rdir))) "ressourcedir" conf
+      httpconf = nullConf{ port = (read (conf ! "port")::Int) }
+  socket <- bindPort httpconf
+  getUserEntryForName (conf ! "user") >>= setUserID . userID
   -- initialisation for gitit-wiki
-  putStrLn $ "Ressource-dir: " ++ (conf' ! "ressourcedir")
   wikiconf <- getWikiConfiguration rdir wikidir
-  putStrLn $ "Wiki-repository: " ++ (repositoryPath wikiconf)
-  putStrLn "HUIS is now ready for takeoff!"
-  simpleHTTP (Conf (read (conf ! "port")::Int) Nothing) (runDispatcher conf' wikiconf)
+  -- initialise database
+  connection <- connectDatabase conf'
+  -- spawn as Daemon
+  createSession
+  forkIO $ simpleHTTPWithSocket socket httpconf (runDispatcher conf' wikiconf)
+  
 
 -- initialises the gitit-wiki and returns the configuration-file
 getWikiConfiguration::String-> String-> IO Network.Gitit.Config
