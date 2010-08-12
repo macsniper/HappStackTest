29b853c9c2c7632f52e9a46be8306fe892085b57
Lukas <lukas@lukas-laptop.(none)>
2010-08-12 12:49:23 +0200

    reminder aktuell

--- src/HUIS/Reminder.hs
+++ src/HUIS/Reminder.hs
@@ -3,9 +3,11 @@ module HUIS.Reminder where
 import Text.XHtml.Transitional hiding (dir,content)
 import Data.Time (formatTime, getCurrentTime, UTCTime)
 import Control.Monad.Trans (MonadIO, liftIO)
+import Control.Monad
 import Happstack.Server
+import Database.HDBC
 
-data Reminder = Reminder{userID :: Int
+data Reminder = Reminder{userID :: Integer
                         ,erstellt :: UTCTime
                         ,bearbeitet :: UTCTime
                         ,erledigen :: UTCTime
@@ -42,10 +44,34 @@ reminderForm =
     , textfield "inhalt" ! [size "30", value ""]
     , label << "Art der Notiz"
     , textfield "art" ! [size "30", value ""]
-    , submit "run" "starten" ]
+    , submit "run" "anlegen" ]
   , thediv << "HUIS-Reminder"
   ]
 
+saveReminderToDb:: (IConnection d, MonadIO m) => d -> Reminder -> m Integer
+saveReminderToDb db reminder = do
+        let query = "INSERT INTO pbv_wiedervl(erstellt_von, erstellt, zuletzt_bearbeitet, zu_erledigen_bis, erledigt, art, kunde)" ++
+                    " VALUES(?, ?, ?, ?, ?, ?, ?)"
+        t <- liftIO getCurrentTime
+        let vals = [toSql (userID reminder), toSql t, toSql t, toSql t, toSql (erledigt reminder),
+                    toSql (art reminder), toSql (content reminder)]
+        liftIO $ withTransaction db $ \d -> run d query vals
+        [[uid]] <- liftIO $ quickQuery db "select last_insert_rowid()" []
+        return (fromSql uid)
 
-
+getReminderFromDb :: (IConnection d, MonadIO m, MonadPlus m)
+                    => d -> Integer -> m Reminder
+getReminderFromDb db uid = do
+        reminders <- liftIO $ handleSqlError $
+                    quickQuery db "SELECT * FROM pbv_wiedervl WHERE id = ?" [toSql uid]
+        case reminders of
+           ([_,tit,ts,synt,cont,jn,dw,wd]:_) ->
+                    return Reminder { userID = uid
+                                    , erstellt = fromSql ts
+                                    , bearbeitet = fromSql synt
+                                    , erledigen = fromSql cont
+                                    , erledigt = fromSql jn
+                                    , art = fromSql dw
+                                    , content = fromSql wd}
+           _ -> mzero
 
