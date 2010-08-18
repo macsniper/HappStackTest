c96c75125f904cbc2da849ed58d826ff532aee8e
Clemens Mayer <macsniper@gmail.com>
2010-08-18 14:32:04 +0200

    merge of Bernd's patch.

--- src/HUIS/Birthday.hs
+++ src/HUIS/Birthday.hs
@@ -15,26 +15,37 @@ data DateRange = DateRange{from :: String,
 instance FromData DateRange where
   fromData = do
   --regex zur datumsüberprüfung^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$
-  dateBeginn <- look "date1"
+  dateBegin <- look "date1"
   dateEnd <- look "date2"
-  return $ DateRange{from = dateBeginn, to = dateEnd}
+  return $ DateRange{from = dateBegin, to = dateEnd}
 
 
 birthdayForm:: [Html]
 birthdayForm =
   [ gui "/birthday" <<
-    [ label << "Date start"
-    , textfield "date1" ! [size "30", value "'yyyy-mm-dd'"]
-    , label << "Date end"
-    , textfield "date2" ! [size "30", value "'yyyy-mm-dd'"]
-    --, label << "Number of years"
-    --, textfield "years" ! [size "30", value "1"]
+    [ label << "Startdatum"
+    , textfield "date1" ! [size "30", value "yyyy-mm-dd"]
+    , label << "Enddatum"
+    , textfield "date2" ! [size "30", value "yyyy-mm-dd"]
     , submit "run" "starten" ]
   , thediv << "HUIS-Birthday"
   ]
 
+mmDdOfDate:: String-> (String,String)
+mmDdOfDate date =
+    let mm'dd= (tail . snd .  break (=='-')) date
+        (mm,_:dd)= break (=='-') mm'dd
+    in (mm,dd)
+
+
 birthdayResult:: Connection-> DateRange-> ServerPart Response
-birthdayResult conn temp = do
-  let querystring = "SELECT pgd_titel, pgd_vorname, pgd_name, pgd_strasse, pgd_plz, pgd_wohnort, (EXTRACT(YEAR FROM CURRENT_DATE)-EXRACT(YEAR FROM pgd_geburtsdatum)) as alter FROM pgd WHERE pgd_geburtsdatum BETWEEN dateBegin AND dateEnd"
+birthdayResult conn dateRange = do
+  let (gebMonMin,gebDayMin) = mmDdOfDate $ from dateRange
+      (gebMonMax,gebDayMax) = mmDdOfDate $ to dateRange
+      querystring = "SELECT pgd_titel, pgd_vornamen, pgd_name, pgd_strasse, pgd_plz, pgd_wohnort"
+                    ++ ", (YEAR(CURRENT)-YEAR(pgd_geburtsdatum)) as alter"
+                    ++ " FROM pgd"
+                    ++ " WHERE ((MONTH(pgd_geburtsdatum) BETWEEN (" ++ gebMonMin ++ " AND " ++ gebMonMax ++ ")"
+                    ++ " AND (DAY(pgd_geburtsdatum) BETWEEN (" ++ gebDayMin ++ " AND " ++ gebDayMax ++ "))"
   result <- liftIO $ handleSqlError $ quickQuery conn querystring []
   queryToHtml [] result birthdayForm
