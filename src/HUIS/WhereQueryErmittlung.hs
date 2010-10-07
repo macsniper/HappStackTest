a73d6c7e234c1a68f77d47b469d8e9c29869b748
Munir <munir@ubuntu.ubuntu-domain>
2010-10-07 16:35:53 +0200

    munir aktuell

--- src/HUIS/WhereQueryErmittlung.hs
+++ src/HUIS/WhereQueryErmittlung.hs
@@ -5,27 +5,29 @@ import Text.ParserCombinators.Parsec
 type SQL = String
 type TextfeldInhalt = String
 
-whereQueryZu:: TextfeldInhalt-> SQL
+whereQueryZu:: TextfeldInhalt→  SQL
 whereQueryZu textfeldInhalt =
     case parse whereQuerySuche "" textfeldInhalt of
-        Left fehlerMeldung-> "Passt nicht: " ++ show fehlerMeldung
-        Right whereQuery-> whereQuery
+        Left fehlerMeldung→  "Passt nicht: " ⊕ show fehlerMeldung
+        Right whereQuery→  whereQuery
 
 whereQuerySuche:: Parser SQL
 whereQuerySuche = do
-    vn <- vorname
-    nn <- try nachname <|> return ""
-    return $ if vn == "" then ""
-           else "WHERE " ++ (if nn=="" then "pgd_vn= '" ++ vn ++ "'"
-                            else "pgd_vn='" ++ vn ++ "', pgd_nn='" ++ nn ++ "'") ++ ";"
+    vn ←  parseFirstname
+    nn ←  try parseLastname <|> return ""
+    return $ if vn ≡ "" then ""
+           else "WHERE " ⊕ (if nn=="" then "pgd_nn= '" ⊕ vn ⊕ "'"
+                            else "pgd_vn='" ⊕ vn ⊕ "', pgd_nn='" ⊕ nn ⊕ "'") ⊕ ";"
 
-vorname:: Parser String
-vorname = do
+parseFirstname:: Parser String
+parseFirstname = do
     try $ skipMany (oneOf " 1234567890")
     many1 letter
 
-nachname:: Parser String
-nachname = do
+parseLastname:: Parser String
+parseLastname = do
     string " "
     many1 letter
+
+
 
