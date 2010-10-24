fb4effdf0a7e7e252944b1897ffd64db81f78bb9
Clemens Mayer <macsniper@gmail.com>
2010-10-24 13:08:04 +0200

    pushed changed by lukas

--- src/HUIS/Reminder.hs
+++ src/HUIS/Reminder.hs
@@ -18,7 +18,10 @@ data Reminder = Reminder{userName :: String
                         ,erledigen :: String
                         ,erledigt :: String
                         ,art :: String
-                        ,content :: String}
+                        ,content :: String
+                        ,datum :: String
+                        ,userGet :: String
+                        ,erledigtGet :: String}
 
 instance FromData Reminder where
   fromData = do
@@ -27,19 +30,25 @@ instance FromData Reminder where
   notiz <- look "inhalt"
   fertig <- lookRead "erledigt"
   notizart <- lookRead "art"
-  return $ Reminder{userName = user, erledigen = frist, erledigt = fertig, content = notiz, art = notizart}
+  datumAbf <- look "datum"
+  userAbf <- look "userGet"
+  erledigtAbf <- look "erledigtGet"
+  return $ Reminder{userName = user, erledigen = frist, erledigt = fertig, content = notiz, art = notizart, datum = datumAbf, userGet = userAbf, erledigtGet = erledigtAbf}
 
 nullReminder :: Reminder
-nullReminder = Reminder{userName = undefined
-                        ,erledigen = undefined
-                        ,erledigt = undefined
-                        ,art = undefined
-                        ,content = "" }
+nullReminder = Reminder{userName = ""
+                        ,erledigen = ""
+                        ,erledigt = ""
+                        ,art = ""
+                        ,content = ""
+                        ,datum = ""
+                        ,userGet=""
+                        ,erledigtGet=""}
 
 
 reminderForm:: [Html]
 reminderForm =
-  [ gui "/Reminder" <<
+  [ gui "/reminder" <<
     [ label << "User"
     , textfield "user" ! [size "30", value ""]
     , br
@@ -53,8 +62,7 @@ reminderForm =
                   , cols "40" ] << [ "Text eingeben" ]
     , br
     , label << "Art der Notiz"
-    , select ! [name "art"] << [ option << p << ""
-                               , option << p << "Telefonnotiz"
+    , select ! [name "art"] << [ option << p << "Telefonnotiz"
                                , option << p << "Gesprächsnotiz"
                                , option << p << "Geburtsurkunde"
                                , option << p << "Elternzeitantrag"
@@ -66,14 +74,19 @@ reminderForm =
                                     ,option << p << "Ja"
                                     ]
     , br
-    , submit "run" "anlegen"
+    , submit "run" "anlegen"]
+    , gui "/reminderGet" <<
+    [ label << "Wiedervorlage anzeigen"
     , br
-    , label << "Wiedervorlage vom "
     , textfield "datum" ! [size "30", value  "JJJJ-MM-TT"]
-    , label << "anzeigen."
     , br
-    , submit "run2" "aufrufen"]
-
+    , textfield "userGet" ! [size "30", value "Bitte Name eingeben"]
+    , br
+    , select ! [name "erledigtGet"] << [option << p << "Nein"
+                                    ,option << p << "Ja"
+                                    ]
+    , br
+    , submit "run" "anzeigen"]
   , thediv << "HUIS-Reminder"
   ]
 
@@ -81,11 +94,11 @@ date :: IO (Integer,Int,Int) -- :: (year,month,day)
 date = getCurrentTime >>= return . toGregorian . utctDay
 
 
-saveReminderToDb:: (IConnection d, MonadIO m) => d -> Reminder -> m Integer
-saveReminderToDb db reminder = do
+saveReminderToDb:: Connection -> Reminder -> ServerPart Response
+saveReminderToDb conn req = do
         let query = "INSERT INTO pbv_wiedervl(erstellt_von, erstellt, zuletzt_bearbeitet, zu_erledigen_bis, erledigt, art, kunde)" ++
-                     " VALUES(" ++ userName req ++ ", current_date,current_date," ++ erledigen req ++ "," ++ erledigt req ++ "," ++ art req ++ "," ++ content req ++ ")"
-         liftIO $ handleSqlError $ run conn query []
+                    " VALUES(" ++ userName req ++ ", current_date,current_date," ++ erledigen req ++ "," ++ erledigt req ++ "," ++ art req ++ "," ++ content req ++ ")"
+        liftIO $ handleSqlError $ run conn query []
         dir "reminder" $ methodSP GET $ showPage "Reminder Query" reminderForm
 
 makeWhereQuery :: Reminder -> String
@@ -108,5 +121,6 @@ reminderResult conn req = do
   let querystring = "SELECT * FROM pbv_wiedervl WHERE TRUE" ++ makeWhereQuery req ++ userGet req ++ erledigtGet req ++ ";"
   result <- liftIO $ handleSqlError $ quickQuery conn querystring []
   queryToHtml [] result reminderForm
+
 
 
